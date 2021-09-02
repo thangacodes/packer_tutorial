47315a3d12edc44fc166d7d8014da69b91de947e
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-09-02 08:45:02 +0530

    Update packer-tf.sh

--- packer-tf.sh
+++ packer-tf.sh
@@ -1,14 +1,9 @@
 ###
 #!/bin/bash
 echo "Shell Script to play around with Packer and invoke Terraform scripts"
-sleep 4
 packer init .
-sleep 2
 packer validate ebs.json
-sleep 2
-packer build ebs.json > /home/centos/apps/packer_tutorial/bake.txt
+packer build ebs.json
 pwd
 ls -lth
-cat /home/centos/apps/packer_tutorial/bake.txt | grep 'ami'
-sleep 2
 exit
