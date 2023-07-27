bd12885baf5b8f658be5bbfb3fae703e61467004
Murugan <thangadurai.murugan@tesco.com>
2023-07-27 14:02:47 +0530

    slight modification

--- packer-tf.sh
+++ packer-tf.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Going to check packer is installed or not"
+echo "The script is executed at: " $(date '+%Y-%m-%d %H:%M:%S')
+
+echo "**** Going to check packer is installed or not *****"
+
 packer --version
 
 echo "Going to check packer installation path"
 
 which packer
 
-echo ************************ Going to execute packer build steps ******************************
+echo "**** Going to execute packer build steps ****"
 
 packer build -var-file=vars.json template.json
+exit
 
